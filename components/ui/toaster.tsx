78287d1297d55390b2987e8511f8c4350e47fc7b
Edonita Berisha <51457583+edonita@users.noreply.github.com>
2025-10-24 17:08:48 +0200

    Remove useToast hook and clean Toaster component

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,23 +1,14 @@
-import { useToast } from "@/hooks/use-toast";
+"use client";
 import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from "@/components/ui/toast";
 
 export function Toaster() {
-  const { toasts } = useToast();
-
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && <ToastDescription>{description}</ToastDescription>}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
+      <Toast>
+        <ToastTitle>Notification</ToastTitle>
+        <ToastDescription>Your portfolio is live!</ToastDescription>
+        <ToastClose />
+      </Toast>
       <ToastViewport />
     </ToastProvider>
   );
